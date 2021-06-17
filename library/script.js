dcc1af18bb92fb51c6aa1316b007d2b4cde71418
Mick <micktoying@gmail.com>
2021-06-17 02:05:51 -0700

    add remove function

--- library/script.js
+++ library/script.js
@@ -1,10 +1,11 @@
 let myLibrary = [];
-
+let idCount = 0;
 function Book(name, pages) {
   // Book constructor
   this.name = name
   this.pages = pages 
   this.added = false;
+  this.id = idCount++
 
 }
 
@@ -18,11 +19,30 @@ function displayBooks() {
     console.log(container)
     myLibrary.forEach(function(e) {
         console.log(e)
-        //add card for each book into page
-        let card = document.createElement('div')
-        card.classList.add('book')
-        card.textContent = 'Title : ' + e['name'] + '\n' + 'Pages : ' + e['pages']
-        container.appendChild(card)
+        if (!e.added) {
+            //add card for each book into page
+            let card = document.createElement('div')
+            card.classList.add('book')
+            card.value = e.id
+            card.textContent = 'Title : ' + e['name'] + '\n' + 'Pages : ' + e['pages'] 
+
+            let removeBtn = document.createElement('button')
+
+            removeBtn.addEventListener('click', function(x) {
+                removeBook(removeBtn.value)
+                container.removeChild(card)
+            })
+
+            removeBtn.textContent = 'X'
+            card.appendChild(removeBtn)
+            removeBtn.classList.add('remove-btn')
+            removeBtn.value = e.id
+            console.log(removeBtn.value)
+            
+            container.appendChild(card)
+            e.added = true
+        }
+    
     })
 }
 function overlayActive() {
@@ -34,7 +54,23 @@ function overlayActive() {
 function off() {
     document.getElementById("overlay").style.display = "none";
     document.getElementById("overlay-container").style.display = "none";
-  }
+}
+
+function removeBook(value) {
+    myLibrary.forEach(function(e) {
+        console.log(e.id, value +' dd')
+        if(e.id == value) {
+            
+            myLibrary.splice(myLibrary.indexOf(e),1)
+        }
+    })
+    // redisplay()fggddf
+}
+
+function redisplay() {
+    container.remove
+}
+
 
 let addBtn = document.getElementById('new-book-btn')
 addBtn.addEventListener('click', function() {
@@ -46,12 +82,23 @@ overlay.addEventListener('click',function() {
     off()
 })
 
-
+let submitBtn = document.getElementById('submit-btn')
+submitBtn.addEventListener('click', function() {
+    console.log('clicked')
+    let formName = document.getElementById('book-name').value
+    let formPages = document.getElementById('book-pages').value
+    console.log(formName,formPages)
+    let newBook = new Book(formName,formPages) 
+    console.log(newBook)
+    addBookToLibrary(newBook)
+    displayBooks()
+    off()
+})
 
 off()
 
 let b1 = new Book("Anna",121)
 addBookToLibrary(b1)
-addBookToLibrary(b1)
+
 
 displayBooks()
